18cf5ccd9a5513f042b31f52c486942c71633ed4
unknown <profjaywon@gmail.com>
2023-06-08 13:13:16 +0100

    changed env

--- cashout/apiConfig.js
+++ cashout/apiConfig.js
@@ -1,6 +1,6 @@
 const API_BASE_URL =
   process.env.NODE_ENV === "production"
-    ? "https://cashout-app.onrender.com"
+    ? "https://cashout-app.cyclic.app"
     : "http://localhost:4000";
 
 export default API_BASE_URL;
