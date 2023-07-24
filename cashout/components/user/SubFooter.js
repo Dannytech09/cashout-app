54dbf6442c8146d629a86b027d7b7afb9b481678
unknown <profjaywon@gmail.com>
2023-07-24 19:29:27 +0100

    whatsapp-w-h

--- cashout/components/user/SubFooter.js
+++ cashout/components/user/SubFooter.js
@@ -10,8 +10,8 @@ export default function SubFooter() {
           <Image
             src="/whatsapp.png"
             alt="whatsapp icon"
-            width="50"
-            height="50"
+            width="40"
+            height="40"
           />
         </Link>
       </div>
