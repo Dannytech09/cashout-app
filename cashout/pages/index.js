ac52a2b99893aadc6be30d872d4e6c842ba72c92
Dannytech09 <profjaywon@gmail.com>
2023-10-20 11:06:31 +0100

    added twk

--- cashout/pages/index.js
+++ cashout/pages/index.js
@@ -1,5 +1,4 @@
-// import { useEffect } from "react";
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+import { useEffect } from "react";
 import Head from "next/head";
 import Hero from "../components/homePage/Hero";
 import ContainerBlock from "@/components/homePage/ContainerBlock";
@@ -7,21 +6,21 @@ import Pricing from "@/components/homePage/Pricing";
 import SubMain from "@/components/homePage/SubMain";
 
 export default function Home() {
-  //   useEffect(() => {
-  //     // <!--Start of Tawk.to Script-->
-  // {/* <script type="text/javascript"> */}
-  // var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-  // (function(){
-  // var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-  // s1.async=true;
-  // s1.src='https://embed.tawk.to/65203706eb150b3fb99ed20e/1hc2tdsvd';
-  // s1.setAttribute('crossorigin','*');
-  // s0.parentNode.insertBefore(s1,s0);
-  // })();
-  // // </script>
-  // // <!--End of Tawk.to Script-->
+    useEffect(() => {
+      // <!--Start of Tawk.to Script-->
+  {/* <script type="text/javascript"> */}
+  var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+  (function(){
+  var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+  s1.async=true;
+  s1.src='https://embed.tawk.to/65203706eb150b3fb99ed20e/1hc2tdsvd';
+  s1.setAttribute('crossorigin','*');
+  s0.parentNode.insertBefore(s1,s0);
+  })();
+  // </script>
+  // <!--End of Tawk.to Script-->
 
-  //   }, [])
+    }, [])
 
   return (
     <div>
